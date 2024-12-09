736b7336135bde64069b6582887de88a0fdd6a05
Makoto Ikami <makoto.ikami@gmail.com>
2024-12-09 23:07:18 +0900

    feat: 商品リストページ作成

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.scss'
+})
+export class ProductListComponent {
+}
