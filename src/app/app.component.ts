e4c80a7a5c404928a6015006c157c7385399748d
Makoto Ikami <makoto.ikami@gmail.com>
2024-12-10 22:49:16 +0900

    feat: 商品詳細ページを作成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./common/navbar/navbar.component";
-import { ProductListComponent } from './product-list/product-list.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, ProductListComponent], 
+  imports: [RouterOutlet, NavbarComponent], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-product-list',
+  imports: [RouterLink],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
