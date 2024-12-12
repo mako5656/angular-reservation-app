b69e6ccb7216529adc9617cddfe3de89adff78d7
Makoto Ikami <makoto.ikami@gmail.com>
2024-12-12 21:33:48 +0900

    refactor: *ngForを使ってリファクタ

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-product-list',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
 export class ProductListComponent {
+  products = [1, 2, 3, 4];
 }
