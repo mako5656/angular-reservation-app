cb0e6e60546025f34fb1837355b29830b23e189d
Makoto Ikami <makoto.ikami@gmail.com>
2024-12-23 22:24:58 +0900

    feat: ProductModuleの作成

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,17 @@
 import { Routes } from '@angular/router';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
+import { ProductComponent } from './product/product.component';
 
 export const routes: Routes = [
-    { path: '', component: ProductListComponent },
-    { path: 'detail', component: ProductDetailComponent }
+    {
+        path: '', redirectTo: 'products', pathMatch: 'full',
+    },
+    {
+        path: 'products', component: ProductComponent,
+        children: [
+            { path: '', component: ProductListComponent },
+            { path: 'detail', component: ProductDetailComponent }
+        ]
+    }
 ];
