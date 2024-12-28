f36bf04e719365ec281c9afe1814ca3e338ea39c
Makoto Ikami <makoto.ikami@gmail.com>
2024-12-28 14:53:24 +0900

    feat: プロダクトを取得するエンドポイントを追加

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const Product = require('../model/product');
 
-router.get('', function(req, res) {
-    res.json({'ok': true});
-})
+router.get('', async (req, res) => {
+    try {
+        const foundProducts = await Product.find({});
+        res.json(foundProducts);
+    } catch (err) {
+        res.status(500).json({ error: 'Something went wrong!' });
+    }
+});
+
+router.get('/:productId', async (req, res) => {
+    try {
+        const productId = req.params.productId;
+        const foundProduct = await Product.findById(productId);
+        res.json(foundProduct);
+    } catch (err) {
+        res.status(500).json({ error: 'Something went wrong!' });
+    }
+});
 
 module.exports = router;
