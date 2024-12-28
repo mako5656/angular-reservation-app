7c8d18665ccddc7f11fbc1e44c5bf92e1e66b21c
Makoto Ikami <makoto.ikami@gmail.com>
2024-12-28 15:37:33 +0900

    refactor: 一覧ページの商品情報をサービス経由で取得するように

--- src/app/product/product-listings/product-listings.component.ts
+++ src/app/product/product-listings/product-listings.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { products } from '../../products';
+import { ProductService } from '../shared/product.service';
 
 @Component({
   standalone: true,
@@ -24,9 +24,9 @@ export class ProductListComponent implements OnInit {
     heading_text3: string;
   }[] = [];
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
-    this.products = products;
+    this.products = this.productService.getProducts();
   }
 }
---
+++ src/app/product/shared/product.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@angular/core";
+import { products } from "../../products";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ProductService {
+
+    getProducts() {
+        return products;
+    }
+}
